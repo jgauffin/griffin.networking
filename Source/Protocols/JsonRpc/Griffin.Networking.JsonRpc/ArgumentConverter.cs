389d0f9e75e18afd6850e37e03407ac7e9b16d79
jgauffin <jonas@gauffin.org>
2012-04-30 12:24:11 +0200

    Some jsonRPC stuff

---
+++ Source/Protocols/JsonRpc/Griffin.Networking.JsonRpc/ArgumentConverter.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Griffin.Networking.JsonRpc
+{
+    public class ArgumentConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object source;
+            string name;
+
+            if (reader.TokenType == JsonToken.PropertyName)
+            {
+                name = (string)reader.Value;
+                reader.Read();
+            }
+
+            //[42, 23]
+            if (reader.TokenType == JsonToken.StartArray)
+            {
+                reader.Read();
+                List<object> values = new List<object>();
+
+                while (reader.TokenType != JsonToken.EndArray)
+                {
+                    values.Add(reader.Value);
+                    reader.Read();
+                }
+
+
+                reader.Read();
+
+                source = values.ToArray();
+            }
+            else //{"subtrahend": 23, "minuend": 42}
+            {
+                reader.Read();
+                var values = new Dictionary<string, object>();
+                while (reader.TokenType != JsonToken.EndObject)
+                {
+                    if (reader.TokenType != JsonToken.PropertyName)
+                        throw new InvalidOperationException("Expected a property name, got: " + reader.TokenType);
+                    string propertyName = reader.Value.ToString();
+                    reader.Read();
+
+                    values.Add(propertyName, reader.Value);
+                    reader.Read();
+                }
+
+                source = values;
+            }
+
+            reader.Read();
+            return source;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
